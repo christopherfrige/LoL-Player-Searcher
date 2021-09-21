cb97db7056e5a3d63865e2fd6b1509f88a7a2224
Christopher Frige <chrisfrige@gmail.com>
2021-09-21 16:28:30 -0300

    changed for env variables

---
+++ utils/EnviromentVariables.py
@@ -0,0 +1,4 @@
+import os
+
+# Set your RIOT API KEY in your env variables
+RIOT_API_KEY = "?api_key=" + os.getenv('RIOT_API_KEY')
--- utils/tokens.py
+++
@@ -1,2 +0,0 @@
-# Change the API_KEY according to yours
-API_KEY = "?api_key=" + "YOUR-RIOT-API-KEY-HERE"
