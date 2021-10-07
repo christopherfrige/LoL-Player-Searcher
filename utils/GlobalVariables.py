59f322b76c405c3a232350510ecaf64f88c88ee9
Christopher Frige <chrisfrige@gmail.com>
2021-10-07 17:06:44 -0300

    adapted for the new Riot API version

--- utils/GlobalVariables.py
+++ utils/GlobalVariables.py
@@ -1,12 +1,16 @@
-# Change the API region if necessary
 # Other regions: br1, eun1, euw1, jp1, kr, la1, la2, na1, oc1, tr1, ru
 
+# The AMERICAS routing value serves NA, BR, LAN, LAS, and OCE. 
+# The ASIA routing value serves KR and JP.
+# The EUROPE routing value serves EUNE, EUW, TR, and RU.
+
 region = 'br1'
+continent = "americas"
 
 URL_PLAYER_DATA = f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
 
 URL_ACTIVE_GAME = f'https://{region}.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/'
 
-URL_MATCH_HISTORY = f'https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/'
+URL_MATCHHISTORY_IDS = f'https://{continent}.api.riotgames.com/lol/match/v5/matches/by-puuid/'
 
-URL_MATCH = f'https://{region}.api.riotgames.com/lol/match/v4/matches/'
+URL_MATCH_DATA = f'https://{continent}.api.riotgames.com/lol/match/v5/matches/'
