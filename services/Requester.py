6ce4dafad497de47ef6ddbd9d6aece8570245893
Christopher Frige <chrisfrige@gmail.com>
2021-10-07 17:05:00 -0300

    refactoring: functions added in Classes

---
+++ services/Requester.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from utils.EnviromentVariables import RIOT_API_KEY
+
+class Requester:
+    def __init__(self):
+        self.header = RIOT_API_KEY
+
+    def get_content(self, url):
+        response = requests.get(
+            url=url,
+            headers=self.header
+        )
+
+        if response.status_code == 200:
+            return json.loads(response.content)
+        
+        return response
+
+if __name__ == "__main__":
+    pegar_data = Requester()
+    print(pegar_data.get_content("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Koshi o Duelista"))
