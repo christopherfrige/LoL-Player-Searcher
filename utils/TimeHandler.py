e7f427b687f45b8068f3cc7a1e8144ac5a98f135
Christopher Frige <chrisfrige@gmail.com>
2021-10-07 17:06:07 -0300

    utilities for the code

---
+++ utils/TimeHandler.py
@@ -0,0 +1,7 @@
+import time
+
+# Return the time (in seconds) since the given timestamp
+def time_since_given_timestamp(timestamp):
+    return time.time() - (timestamp)
+
+print(time_since_given_timestamp(1633571299695))
