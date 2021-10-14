fb92b4515e2ff08fd1b35e1ba93bf34b8fa2e13b
Christopher Frige <chrisfrige@gmail.com>
2021-10-14 16:43:49 -0300

    flask added to generate a mini API

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from main import main
+app = Flask(__name__)
+
+@app.route('/')
+def data():
+    players = main()
+    return jsonify(players)
+
+if __name__ == "__main__":
+    app.run(debug=True)
